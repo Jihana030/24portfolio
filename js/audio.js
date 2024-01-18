90ba468a23e84ee9c1e3cc6bed7fa5b1bae5d543
jiwon <wldnjs7618@naver.com>
2024-01-18 17:36:35 +0900

    audio js event try..

--- js/audio.js
+++ js/audio.js
@@ -2,21 +2,22 @@
     'use strict';
     const prfAudio = document.querySelector('#prf_audio');
     const playBtn = document.querySelector('#play_btn');
-    const prfImg = document.querySelector('#prf_img>img');
-
-    // audio play
-    playBtn.addEventListener('click', e => {
-        prfAudio.play();
-        playBtn.classList.add('st_opacity');
-        prfImg.classList.add('cursor_p');
-    })
-
-    // audio pause
-    if (!(playBtn.classList.contains('st_opacity'))) {
-        prfImg.addEventListener('click', e=>{
+    const audioWrap = document.querySelector('.audio_wrap');
+    const audioChild = audioWrap.lastElementChild;
+    console.log(audioChild)
+    if (audioChild.classList.contains('st_opacity')) {
+        // audio pause
+        audioChild.addEventListener('click', e => {
             prfAudio.pause();
-            playBtn.classList.remove('st_opacity');
-            prfImg.classList.remove('cursor_p');
+            audioChild.classList.remove('st_opacity');
+            console.log('pause');
+        })
+    } else {
+        // audio play
+        audioChild.addEventListener('click', e => {
+            prfAudio.play();
+            audioChild.classList.add('st_opacity');
+            console.log('play');
         })
     }
 })();
