a61e4bd7d0d41bf465d4df47355e7938cad99d23
jiwon <wldnjs7618@naver.com>
2024-01-18 17:47:40 +0900

    audio event sol

--- js/audio.js
+++ js/audio.js
@@ -2,22 +2,19 @@
     'use strict';
     const prfAudio = document.querySelector('#prf_audio');
     const playBtn = document.querySelector('#play_btn');
-    const audioWrap = document.querySelector('.audio_wrap');
-    const audioChild = audioWrap.lastElementChild;
-    console.log(audioChild)
-    if (audioChild.classList.contains('st_opacity')) {
-        // audio pause
-        audioChild.addEventListener('click', e => {
+    
+    // audio pause
+    playBtn.addEventListener('click', e => {
+        if (playBtn.classList.contains('st_opacity')) {
             prfAudio.pause();
-            audioChild.classList.remove('st_opacity');
+            playBtn.classList.remove('st_opacity');
             console.log('pause');
-        })
-    } else {
-        // audio play
-        audioChild.addEventListener('click', e => {
+        } else {
+            // audio play
             prfAudio.play();
-            audioChild.classList.add('st_opacity');
+            playBtn.classList.add('st_opacity');
             console.log('play');
-        })
-    }
+        }
+    })
+
 })();
