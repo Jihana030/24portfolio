02b5d180e5278dc46a2595837a512aaaca87ff36
jiwon <wldnjs7618@naver.com>
2024-02-07 17:31:47 +0900

    react study 240207

---
+++ reactstudy/dicegame/src/App.js
@@ -0,0 +1,13 @@
+import Dice from './Dice';
+
+function App() {
+  return (
+    <div>
+      <Dice color='red' num={2}/>
+    </div>
+  );
+}
+
+export default App;
+
+/* 속성에 숫자도 중괄호로 감싸줘야함 */
--- reactstudy/dicegame/src/index.js
+++ reactstudy/dicegame/src/index.js
@@ -1,21 +1,22 @@
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const product = 'Macbook';
-const model = 'Air';
-const item = product + model;
-
 root.render(
-  <h1>안녕 {item}</h1>,
+  <App/>,
   document.getElementById('root')
 );
 
-// className
-// htmlFor
-// 속성이름은 카멜케이스로.
-// JSX는 self-closing 필수
-// 부모 하나 안에 모든 jsx 코드가 있어야함.
-// Fragment를 활용하면, 혹은 빈 부모태그를 사용하면 부모가 불필요할때 보이지않게 할 수 있다.
-// 이벤트 핸들러는 onclick 사용. 중괄호로 함수명 사용.
-// 중괄호 안에는 변수만 사용가능. for문이나 if문 사용 불가.
+/*
+  className
+  htmlFor
+  속성이름은 카멜케이스로.
+  JSX는 self-closing 필수
+  부모 하나 안에 모든 jsx 코드가 있어야함.
+  Fragment를 활용하면, 혹은 빈 부모태그를 사용하면 부모가 불필요할때 보이지않게 할 수 있다.
+  이벤트 핸들러는 onclick 사용. 중괄호로 함수명 사용.
+  중괄호 안에는 변수만 사용가능. for문이나 if문 사용 불가.
+  컴포넌트는 대문자로 시작, 내부는 JSX문법이어야함.
+  이미지를 데려올때는 import diceBlue01 from './assets/dice-blue-1.svg';
+  컴포넌트의 속성 = props(프로퍼티에서 따온 말)
+*/
