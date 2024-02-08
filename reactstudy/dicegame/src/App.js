eba2110a1d93f100d1dfc0973e85173eae6d76af
Jihana <wldnjs7618@naver.com>
2024-02-08 17:47:09 +0900

    reactstudy 240208

--- reactstudy/dicegame/src/App.js
+++ reactstudy/dicegame/src/App.js
@@ -1,8 +1,13 @@
+import Button from './Button';
 import Dice from './Dice';
 
 function App() {
   return (
     <div>
+      <div>
+        <Button>던지기</Button>
+        <Button>처음부터</Button>
+      </div>
       <Dice color='red' num={2}/>
     </div>
   );
@@ -12,4 +17,5 @@ export default App;
 
 /* 
   속성에 숫자도 중괄호로 감싸줘야함 
+  단순히 보여주기만 할 때는 children prop(문자열, 컴포넌트, 다른 태그)을 활용하는게 좋다.
 */
