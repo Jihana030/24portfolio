74c0809bc299ef5e07078a5363391270c603481e
jiwon <wldnjs7618@naver.com>
2024-02-22 16:02:00 +0900

    react 240222 textmiss modi

--- reactstudy/moviepedia/src/components/App.js
+++ reactstudy/moviepedia/src/components/App.js
@@ -3,7 +3,7 @@ import items from "../mock.json";
 import { useState } from "react";
 
 function App() {
-  const [order, setOrder] = useState('creatAt');
+  const [order, setOrder] = useState('createdAt');
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder('createdAt');
