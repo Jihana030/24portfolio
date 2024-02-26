ad21cb686a3d5f9123e095e2fb7494fe25645354
jiwon <wldnjs7618@naver.com>
2024-02-26 14:33:27 +0900

    240226 react fetch async

---
+++ reactstudy/moviepedia/src/api.js
@@ -0,0 +1,8 @@
+// 리퀘스트 함수들을 모아 놓고 사용
+
+// fetch를 호출하고 받아온 리스폰스 body를 리턴하는 함수
+export async function getReviews() {
+    const response = await fetch("https://learn.codeit.kr/api/film-reviews");
+    const body = await response.json();
+    return body;
+}
