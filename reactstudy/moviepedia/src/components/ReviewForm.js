e6e41a4811c612c717a7daf00c5a212a4f48cb3a
jiwon <wldnjs7618@naver.com>
2024-03-06 15:32:22 +0900

    react 240306 input onChang

--- reactstudy/moviepedia/src/components/ReviewForm.js
+++ reactstudy/moviepedia/src/components/ReviewForm.js
@@ -6,15 +6,28 @@ function ReviewForm() {
 
   // 영화 제목을 받을 state
   const [title, setTitle] = useState("");
+  const [rating, setRating] = useState(0);
+  const [content, setContent] = useState("");
 
   const handleTitleChange = (e) => {
     // input의 value가 변경될 때마다 그 값을 state에 반영
     setTitle(e.target.value);
   };
 
+  const handleRatingChange = (e) => {
+    const nextRating = Number(e.target.value) || 0;
+    setRating(nextRating);
+  };
+
+  const handleContentChange = (e) => {
+    setContent(e.target.value);
+  }
+
   return (
     <form>
-      <input value={title} onChange={handleTitleChange}></input>
+      <input value={title} onChange={handleTitleChange} />
+      <input type="number" value={rating} onChange={handleRatingChange} />
+      <textarea value={content} onChange={handleContentChange} />
     </form>
   );
 }
