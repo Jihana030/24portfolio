a13b1858184121fe2d97e6c12e478d25f331eb61
jiwon <wldnjs7618@naver.com>
2024-04-03 16:33:52 +0900

    router set

--- study/src/Main.js
+++ study/src/Main.js
@@ -1,11 +1,14 @@
-import App from './components/App';
-import HomePage from './pages/HomePage';
+import App from "./components/App";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter } from 'react-router-dom';
 
 function Main() {
   return (
-    <App>
-      <HomePage />
-    </App>
+    <BrowserRouter>
+      <App>
+        <HomePage />
+      </App>
+    </BrowserRouter>
   );
 }
 
